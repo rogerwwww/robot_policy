9512c2e5af6a16f9df740737c628128f149e3af6
Roger Wang <liberatetheus@hotmail.com>
2018-01-19 15:57:37 +0800

    Add super naive FSM

---
+++ state_machines/norm_attack.py
@@ -0,0 +1,234 @@
+from state_machines.path_planning import PathPlanning
+import numpy as np
+
+# Arrive at ROI threshold
+ROI_THRESH = 5
+# Single step length
+STEP_LEN = 5
+# Distance when close to target enough
+CLOSE_THRESH = 30
+# When closer than this distance, will shot when pursuit
+PURSUIT_SHOT_DIST = 50
+
+
+class NormAttack():
+    """
+    Norm Attack Finite State Machine
+    """
+    # Element = None if unknown
+    def __init__(self, self_id, self_info, enemy_info, init_state='start'):
+        # ID of current robot
+        self.self_id = self_id
+        # States
+        self.last_state = None
+        self.cur_state = init_state
+        # Self and enemy information
+        self.last_self_info = None
+        self.self_info = np.array(self_info)
+        self.last_enemy_info = None
+        self.enemy_info = np.array(enemy_info)
+        # Path planning instance
+        self.path_planner = None
+        # Target enemy robot ID
+        self.target_id = 0
+
+    def run(self, self_info, enemy_info):
+        """
+        Run the norm attack state machine once.
+        :param self_info: numpy array, input information of self
+        :param enemy_info: numpy array, input information of enemy, None if element unknown
+        :return: action array of this robot
+        """
+        # Update info
+        self.last_self_info = self.self_info
+        self.last_enemy_info = self.enemy_info
+        self.self_info = np.array(self_info)
+        self.enemy_info = np.array(enemy_info)
+
+        # Run state machine
+        act = self.state_methods[self.cur_state](self)
+        return act
+
+    def run_start(self):
+        """
+        Run start state once.
+        :return: action array of this robot
+        """
+        # State transition
+        if not self.enemy_sighted:
+            next_state = 'patrol'
+        elif not self.target_is_moving and self.close_to_target_enough:
+            next_state = 'dps'
+        else:
+            next_state = 'pursuit'
+
+        # Action
+        if next_state == 'patrol':
+            act = self.patrol_action(init=True)
+        elif next_state == 'dps':
+            act = self.dps_action()
+        else:
+            act = self.pursuit_action(init=True)
+
+        # Perform state transition
+        self.last_state = self.cur_state
+        self.cur_state = next_state
+        return list(act)
+
+    def run_patrol(self):
+        """
+        Run patrol state once.
+        :return: action array of this robot
+        """
+        # State transition
+        if not self.enemy_sighted:
+            next_state = 'patrol'
+        elif not self.target_is_moving and self.close_to_target_enough:
+            next_state = 'dps'
+        else:
+            next_state = 'pursuit'
+
+        # Action
+        if next_state == 'patrol':
+            act = self.patrol_action()
+        elif next_state == 'dps':
+            act = self.dps_action()
+        else:
+            act = self.pursuit_action(init=True)
+
+        # Perform state transition
+        self.last_state = self.cur_state
+        self.cur_state = next_state
+        return list(act)
+
+    def run_pursuit(self):
+        """
+        Run pursuit state once.
+        :return: action array of this robot
+        """
+        # State transition
+        if not self.enemy_sighted:
+            next_state = 'patrol'
+        elif not self.target_is_moving and self.close_to_target_enough:
+            next_state = 'dps'
+        else:
+            next_state = 'pursuit'
+
+        # Action
+        if next_state == 'patrol':
+            act = self.patrol_action(init=True)
+        elif next_state == 'dps':
+            act = self.dps_action()
+        else:
+            act = self.pursuit_action()
+
+        # Perform state transition
+        self.last_state = self.cur_state
+        self.cur_state = next_state
+        return list(act)
+
+    def run_dps(self):
+        """
+        Run DPS state once.
+        :return: action array of this robot
+        """
+        # State transition
+        if not self.enemy_sighted:
+            next_state = 'patrol'
+        elif self.close_to_target_enough:
+            next_state = 'dps'
+        else:
+            next_state = 'pursuit'
+
+        # Action
+        if next_state == 'patrol':
+            act = self.patrol_action(init=True)
+        elif next_state == 'dps':
+            act = self.dps_action()
+        else:
+            act = self.pursuit_action(init=True)
+
+        # Perform state transition
+        self.last_state = self.cur_state
+        self.cur_state = next_state
+        return list(act)
+
+    def patrol_action(self, init=False):
+        """
+        Plan and command patrol action for robot.
+        :param init: whether to initiate path planner.
+        :return: action array of this robot
+        """
+        if init:
+            f = lambda x, y: (np.ceil((x - y / 2) / y) - 0.5) * 2
+            dst = np.array([f(self.self_position[self.self_id][0], 800) * -300 + 400,
+                            f(self.self_position[self.self_id][1], 500) * -200 + 250])
+            self.path_planner = PathPlanning(self.self_position[self.self_id], dst)
+        elif np.linalg.norm(self.self_position[self.self_id] - self.path_planner.get_dst) < ROI_THRESH:
+            f = lambda x, y: (np.ceil((x - y / 2) / y) - 0.5) * 2
+            dst = np.array([f(self.self_position[self.self_id][0], 800) * -300 + 400,
+                            f(self.self_position[self.self_id][1], 500) * -200 + 250])
+            self.path_planner.update_dst(dst)
+
+        act = self.path_planner.run(self.self_position[self.self_id], STEP_LEN)
+        return act
+
+    def pursuit_action(self, init=False):
+        """
+        Plan and command pursuit action for robot.
+        :param init: whether to initiate the path planner
+        :return: action array of this robot
+        """
+        # Update target
+        dist0 = np.linalg.norm(self.enemy_position[0] - self.self_position[self.self_id])
+        dist1 = np.linalg.norm(self.enemy_position[1] - self.self_position[self.self_id])
+        self.target_id = np.argmin([dist0, dist1])
+
+        # Pursuit target
+        if init:
+            self.path_planner = PathPlanning(self.self_position[self.self_id], self.enemy_position[self.target_id])
+        self.path_planner.update_dst(self.enemy_position[self.target_id])
+        act = self.path_planner.run(self.self_position[self.self_id], STEP_LEN)
+        if np.fmin(dist0, dist1) < PURSUIT_SHOT_DIST:
+            act[-1] = 1
+        return act
+
+    def dps_action(self):
+        """
+        Command dps action for robot.
+        :return: action array of this robot
+        """
+        return [0,0,0,1,0,0,0,0,0,0,1,0,0,0,1]
+
+    @property
+    def self_position(self):
+        return self.self_info[0:2, 0:2]
+
+    @property
+    def enemy_position(self):
+        return self.enemy_info[0:2, 0:2]
+
+    @property
+    def last_enemy_position(self):
+        if self.last_enemy_info is not None:
+            return self.last_enemy_info[0:2, 0:2]
+        else:
+            return None
+
+    @property
+    def enemy_sighted(self):
+        return self.enemy_info[0:2, 0:2].any()
+
+    @property
+    def target_is_moving(self):
+        if self.last_enemy_position is None:
+            return False
+        else:
+            return (self.last_enemy_position[self.target_id] - self.enemy_position[self.target_id]).any()
+
+    @property
+    def close_to_target_enough(self):
+        # TODO: use graph-based distance
+        return np.linalg.norm(self.enemy_position[self.target_id] - self.self_position[self.self_id]) < CLOSE_THRESH
+
+    state_methods = {'patrol': run_patrol, 'pursuit': run_pursuit, 'dps': run_dps, 'start': run_start}
---
+++ state_machines/path_planning.py
@@ -0,0 +1,73 @@
+import numpy as np
+
+
+class PathPlanning:
+    """
+    Path Planning Method class
+    """
+    def __init__(self, cur_pos, dst_pos):
+        self.cur_pos = cur_pos
+        self.dst_pos = dst_pos
+
+    def update_dst(self, update_pos):
+        """
+        Update destination position
+        :param update_pos: updated destination position
+        :return: None
+        """
+        self.dst_pos = update_pos
+
+    @property
+    def get_dst(self):
+        """
+        Get current destination
+        :return: destination coordinate
+        """
+        return self.dst_pos
+
+    def naive(self, step):
+        """
+        Naive path planning method. It takes a straight line towards the destination.
+        :param step: step length
+        :return: a movement in two directions
+        """
+        return (self.dst_pos - self.cur_pos) / np.linalg.norm(self.dst_pos - self.cur_pos) * step
+
+    # Algorithm dictionary
+    algorithms = {'naive': naive}
+
+    def run(self, cur_pos, step, strategy='naive'):
+        """
+        Run the path planning once
+        :param cur_pos: input current position
+        :param step: input step length
+        :param strategy: input destination position
+        :return: a movement in two directions
+        """
+        self.cur_pos = cur_pos
+        act = self.algorithms[strategy](self, step)
+        act = np.array(np.round(act), dtype=int)
+        for i, a in enumerate(act):
+            if a < -3:
+                act[i] = -3
+            elif a > 3:
+                act[i] = 3
+        ret_act = np.zeros(15, dtype=int)
+        ret_act[act[0] + 3] = 1
+        ret_act[act[1] + 3 + 7] = 1
+
+        return ret_act
+
+    # TODO: Add A-star/Dijkstra
+
+
+if __name__ == '__main__':
+    cur_pos = np.array([0, 0])
+    dst_pos = np.array([120, 50])
+    pp = PathPlanning(cur_pos, dst_pos)
+
+    while (cur_pos != dst_pos).all():
+        dp = pp.run(cur_pos, 3)
+        dp = np.array(np.ceil(dp), dtype=int)
+        cur_pos += dp
+        print(cur_pos)
